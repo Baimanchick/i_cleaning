d78cbd63a1c9dbcaf19fc95476846de83c210d58
Baimanchick <baika132213@gmail.com>
2024-07-26 15:24:46 +0600

    added logo images, other changes

--- src/components/VideoSeries/VideoSeries.tsx
+++ src/components/VideoSeries/VideoSeries.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
 import { Tabs } from "antd";
 import styles from "./VideoSeries.module.scss";
 import { Flex } from 'antd';
@@ -13,9 +12,20 @@ import whatsapp from "@/assets/svgs/b2b_section/whatsapp.svg"
 import { VideoType } from '@/helpers/interfaces/video.interface';
 import { getClassVideoByIndex } from '@/helpers/functions/getClassVideoByIndex';
 import { VideoSeriesProps } from './VideoSeries.props';
+import ModalApp from '@/service_components/ModalApp/ModalApp';
+
 
 function VideoSeries({ video, loading }: VideoSeriesProps) {
     const [activeKey, setActiveKey] = useState<string | null>(null);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     useEffect(() => {
         if (video && video.length > 0) {
@@ -88,48 +98,51 @@ function VideoSeries({ video, loading }: VideoSeriesProps) {
     const activeCustomerTitle = video?.find((item: VideoType) => item.id?.toString() === activeKey)?.title;
 
     return (
-        <div className={styles.videoComponent}>
-            <div className='container'>
-                <Flex className={styles.main_title_container} justify={'center'}>
-                    <h2 className={styles.title}>VIDEO SERIES</h2>
-                </Flex>
-            </div>
-
-            <div className={styles.tab_container}>
-                <Flex style={{ width: '100%' }} className={styles.tabs_main} justify={'space-between'}>
-                    <Tabs className={styles.Tabs} centered={true} items={customerMapped} onChange={onTabChange} />
-                </Flex>
-            </div>
-            <div className='container'>
-                <Flex style={{ flexDirection: 'column' }}>
-                    <Flex gap={20} style={{ flexDirection: 'column' }}>
-                        <h2 className={styles.secons_title}>FOR {activeCustomerTitle}</h2>
-                        <p className={styles.text}>
-                            Our company's expertise extends beyond just cleaning services; we prioritize video satisfaction and tailor our approach to meet the unique needs of each client. Additionally, our commitment to sustainability sets us apart, as we strive to minimize our environmental footprint while delivering exceptional results. In Dubai's competitive market, our dedication to professionalism, eco-friendliness, and personalized service makes us the ideal partner for companies seeking reliable and environmentally conscious cleaning solutions.
-                        </p>
+        <>
+            <div className={styles.videoComponent}>
+                <div className='container'>
+                    <Flex className={styles.main_title_container} justify={'center'}>
+                        <h2 className={styles.title}>VIDEO SERIES</h2>
                     </Flex>
-                    <Flex className={styles.utils} align={'center'} justify={'space-between'}>
-                        <div>
-                            <Button className={styles.btn} appearance='blue'>Application</Button>
-                        </div>
+                </div>
+
+                <div className={styles.tab_container}>
+                    <Flex style={{ width: '100%' }} className={styles.tabs_main} justify={'space-between'}>
+                        <Tabs className={styles.Tabs} centered={true} items={customerMapped} onChange={onTabChange} />
+                    </Flex>
+                </div>
+                <div className='container'>
+                    <Flex style={{ flexDirection: 'column' }}>
                         <Flex gap={20} style={{ flexDirection: 'column' }}>
-                            <Flex justify={'flex-end'} gap={12}>
-                                <Flex onClick={() => handleRedirect('https://www.instagram.com/icleaning_dubai/')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
-                                    <img src={instagram.src} alt="instagram" />
+                            <h2 className={styles.secons_title}>FOR {activeCustomerTitle}</h2>
+                            <p className={styles.text}>
+                                Our company's expertise extends beyond just cleaning services; we prioritize video satisfaction and tailor our approach to meet the unique needs of each client. Additionally, our commitment to sustainability sets us apart, as we strive to minimize our environmental footprint while delivering exceptional results. In Dubai's competitive market, our dedication to professionalism, eco-friendliness, and personalized service makes us the ideal partner for companies seeking reliable and environmentally conscious cleaning solutions.
+                            </p>
+                        </Flex>
+                        <Flex className={styles.utils} align={'center'} justify={'space-between'}>
+                            <div>
+                                <Button onClick={showModal} className={styles.btn} appearance='blue'>Application</Button>
+                            </div>
+                            <Flex gap={20} style={{ flexDirection: 'column' }}>
+                                <Flex justify={'flex-end'} gap={12}>
+                                    <Flex onClick={() => handleRedirect('https://www.instagram.com/icleaning_dubai/')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
+                                        <img src={instagram.src} alt="instagram" />
+                                    </Flex>
+                                    <Flex onClick={() => handleRedirect('https://t.me/icleaning_dubai')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
+                                        <img src={telegram.src} alt="telegram" />
+                                    </Flex>
+                                    <Flex onClick={() => handleRedirect('https://api.whatsapp.com/send/?phone=971508648401&text&type=phone_number&app_absent=0')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
+                                        <img src={whatsapp.src} alt="whatsapp" />
+                                    </Flex>
                                 </Flex>
-                                <Flex onClick={() => handleRedirect('https://t.me/icleaning_dubai')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
-                                    <img src={telegram.src} alt="telegram" />
-                                </Flex>
-                                <Flex onClick={() => handleRedirect('https://api.whatsapp.com/send/?phone=971508648401&text&type=phone_number&app_absent=0')} align={'center'} style={{ cursor: 'pointer' }} gap={8}>
-                                    <img src={whatsapp.src} alt="whatsapp" />
-                                </Flex>
+                                <div className={styles.contact}>Contact info: +971508648401</div>
                             </Flex>
-                            <div className={styles.contact}>Contact info: +971508648401</div>
                         </Flex>
                     </Flex>
-                </Flex>
+                </div>
             </div>
-        </div>
+            <ModalApp handleCancel={handleCancel} isModalVisible={isModalVisible} />
+        </>
     );
 }
 
