670af31d42a2925b742a3e26ad4870c38dd9309f
Baimanchick <baika132213@gmail.com>
2024-07-29 19:11:11 +0600

    edit congig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["https://api-icleaning.online"], //make it 'your-domain.com'
+  },
+};
 
 export default nextConfig;
