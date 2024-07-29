4abc52d0c7870448d2e2a679c9d2e01ac374f5cc
Baimanchick <baika132213@gmail.com>
2024-07-29 15:53:24 +0600

    added logo

--- src/app/service/layout.tsx
+++ src/app/service/layout.tsx
@@ -5,15 +5,19 @@ import { StoreProvider } from '@/store/StoreProvider';
 import Navbar from '@/components/Navbar/Navbar';
 import SearchInput from '@/components/SearchInput/SearchInput';
 import Footer from '@/components/Footer/Footer';
+import Head from 'next/head';
 
 export const metadata = {
     title: 'I-Cleaning Service',
 };
 
-const RootLayout = ({ children }: React.PropsWithChildren) => {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 
     return (
         <html lang="en">
+            <Head>
+                <link rel="icon" href="/public/favicon.ico" sizes='any' />
+            </Head>
             <body suppressHydrationWarning={true}>
                 <StoreProvider>
                     <div className="container">
